23995e01b81a6f2ece546033271acfbb5d0b454c
ShimboAkane <47874214+ShimboAkane@users.noreply.github.com>
2019-02-22 11:22:51 +0900

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController: MonoBehaviour {
+    Rigidbody rb;
+    float jamp = 500.0f;
+    public float speed = 3.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)&&this.rb.velocity.y==0)
+        {
+            this.rb.AddForce(transform.up * this.jamp);
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+        if (transform.position.y < -10)
+        {
+            SceneManager.LoadScene("GameOverScene");
+        }
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("ゴール");
+        SceneManager.LoadScene("ClearScene");
+    }
+
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Test : MonoBehaviour {
+    // speedを制御する
+    public float speed = 3.0f;
+    public float a_speed = 1.0f;
+    float moveX = 0f;
+    float moveZ = 0f;
+    float a_moveX = 0f;
+    float a_moveZ = 0f;
+    private CharacterController controller;
+    private Vector3 moveDirection;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        moveX = Input.GetAxis("Horizontal") * speed;
+        moveZ = Input.GetAxis("Vertical") * speed;
+        Vector3 direction = new Vector3(moveX, 0, 0);
+
+        a_moveX = Input.GetAxis("Horizontal") * a_speed;
+        a_moveZ = Input.GetAxis("Vertical") * a_speed;
+        Vector3 a_direction = new Vector3(a_moveX, 0, 0);
+
+        controller.SimpleMove(direction);
+
+        if (controller.isGrounded)
+        { //地面についているか判定
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                moveDirection.y = 15; //ジャンプするベクトルの代入
+            }
+            
+        }
+        moveDirection.y -= 10 * Time.deltaTime; //重力計算
+        controller.Move(moveDirection * Time.deltaTime); //cubeを動かす処理
+
+        if (!controller.isGrounded)
+        {
+            Debug.Log("空中");
+            controller.Move(a_direction);
+        }
+
+        if (transform.position.y < 0)
+        {
+            SceneManager.LoadScene("GameOverScene");
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("ゴール");
+        SceneManager.LoadScene("ClearScene");
+    }
+
+}
